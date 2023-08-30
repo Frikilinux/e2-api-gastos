f42276210d58efc7096a93bd7e988fbaede63feb
Frikilinux <frikilinux@gmail.com>
2023-08-29 23:51:11 -0300

    Change url

--- dataBase/config.ts
+++ dataBase/config.ts
@@ -2,13 +2,13 @@ import mongoose from 'mongoose'
 
 const connectDB = async (): Promise<void> => {
   try {
-    const DBURL = process.env.DB_URL
-    if (!DBURL) {
-      throw new Error('URL is required')
-    }
+    // const DBURL = process.env.DB_URL
+    // if (!DBURL) {
+    //   throw new Error('URL is required')
+    // }
     await mongoose.connect(
-      DBURL
-      // 'mongodb+srv://TestUser:7JlFCstTg5Eff8nW@appgastos.7u6h31l.mongodb.net/?retryWrites=true&w=majority'
+      // DBURL
+      'mongodb+srv://TestUser:7JlFCstTg5Eff8nW@appgastos.7u6h31l.mongodb.net/?retryWrites=true&w=majority'
     )
     console.log('Database connected')
   } catch (error) {
